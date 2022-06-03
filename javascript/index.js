4277913b00e58bc1a10c310578be475649e28333
Matias Carballo <carballomatiasr@gmail.com>
2022-06-02 23:29:13 -0300

    Se carga primera version del proyecto de JS: se crearon los archivos HTML, CSS, SASS, JS y se suben al repositorio remoto

---
+++ javascript/index.js
@@ -0,0 +1,12 @@
+
+
+let coleccion = prompt("ya tienes una figura?")
+
+switch(coleccion) {
+    case "si":
+        alert("Bienvenido hermano ateniense")
+    break;
+    case "no": 
+        alert("Únete este camino sin retorno")
+    break;
+}
