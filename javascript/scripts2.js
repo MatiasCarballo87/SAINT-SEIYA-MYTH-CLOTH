862a4e8c9c0f778d5f73e7e2b0b279abe99e42dd
Matias Carballo <carballomatiasr@gmail.com>
2022-08-01 23:42:25 -0300

    Version final del proyecto

--- javascript/scripts2.js
+++ javascript/scripts2.js
@@ -36,7 +36,6 @@
 fetch('./figures.json')
     .then(response => response.json())
         .then(data => {
-            console.log(data);
             figures = data;
             mostrarMythCloths();
     })
@@ -44,7 +43,6 @@ fetch('./figures.json')
 
 
 //MOSTRAR PRODUCTOS
-
 let cart = JSON.parse(localStorage.getItem('cart')) || []
 
 function mostrarMythCloths() {
@@ -70,14 +68,15 @@ function mostrarMythCloths() {
 
     for (let i = 0; i < addButton.length; i++) {
         const element = addButton[i];
-        element.addEventListener('click', añadirAlCarrito)
+        element.addEventListener('click', anadirAlCarrito)
     }
-    
-}
+    viewCart()
+} 
+
 
 //AÑADIR AL CARRITO
 
-function añadirAlCarrito(e) {
+function anadirAlCarrito(e) {
     const btn = e.target;
     const id = btn.getAttribute('id')
     Swal.fire({
@@ -95,6 +94,8 @@ function añadirAlCarrito(e) {
     }
     console.log(cart)
     localStorage.setItem('cart', JSON.stringify(cart))   
+    showCart.innerHTML = ``
+    viewCart()
 }
 
 
@@ -104,41 +105,62 @@ const totalCart = () => {
     return cart.reduce((acc, figu) => acc + figu.price * figu.cantidad, 0)
 }
 
-
 //MOSTRAR CARRITO
 
 const showCart = document.getElementById('contenedorCart')
 
 
 function viewCart() {
+    const tableContent = `
+    <div class="table">
+        <table>
+            <thead>
+                <tr>
+                    <th>PRODUCTO</th>
+                    <th>NOMBRE DEL PRODUCTO</th>
+                    <th>PRECIO</th>
+                    <th>CANTIDAD</th>
+                    <th>SUBTOTAL</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody id="tbody">
+            </tbody>
+            <tfoot>
+                <tr>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th>TOTAL:</th>
+                    <th>u$d ${totalCart()}</th>
+                    <th><button class="buyCart">COMPRAR</button></th>
+                </tr>
+            </tfoot>
+        </table>
+    </div>   
+    `
+    showCart.innerHTML += tableContent;
+
+    const tbody = document.getElementById('tbody')
+
     for (let i = 0; i < cart.length; i++) {
         const element = cart[i];
         const { id, img, name, price, cantidad} = element
         const selectFig = `
-        <div class="contFiguras">
-            <div class="miniFigura">
-                <img src="${img}">
-            </div>
-            <div class="miniName">    
-                <h3>${name}</h3>
-            </div>
-            <div class="miniPrice">    
-                <h3>u$d ${price}</h3>
-            </div>
-            <div class="cant">
-                <p>${cantidad}</p>
-            </div>
-            <div class="subtotal">
-                <p>$${(cantidad * price)}</p>
-            </div>
-            <div class="delete">
-                <button id=${id} class="deleteButton">Eliminar del carrito</button>
-            </div>  
-        </div>        
+            <tr>
+                <td><img src="${img}"></td>
+                <td>${name}</td>
+                <td>u$d ${price}</td>
+                <td>${cantidad}</td>
+                <td>u$d${(cantidad * price)}</td>
+                <td><button id=${id} class="deleteButton">ELIMINAR</button></td>
+            </tr>
         `
-        showCart.innerHTML += selectFig;
+        
+        tbody.innerHTML += selectFig;
         
     }
+
 }
     
     
@@ -156,14 +178,81 @@ document.addEventListener("click", (e) => {
 function eliminarDelCarrito (e) {
     const delbtn = e.target;
     const id = delbtn.getAttribute('id')
-    const figuFinded = figures.find(figu => figu.id == id)
-    const inCart = cart.find(figu => figu.id == figuFinded.id)
-    console.log(inCart)
-    if(!inCart) {
-        let cartFilted = cart.filter(figu => figu.id != inCart.id)
-        cart = [...cartFilted] && console.log(cart)
-    }
-
-    showCart.innerHTML = ``
-    localStorage.setItem('cart', JSON.stringify(cart))
+    Swal.fire({
+        title: 'Eliminaste a: ' + id + ' del carrito',
+        confirmButtonColor: 'green',
+    })
+    const cartFiltered = cart.filter(figure => figure.id !=id)
+    localStorage.setItem('cart', JSON.stringify(cartFiltered))
+    delbtn.parentNode.parentNode.remove()
+    console.log(cart)
+    cart = JSON.parse(localStorage.getItem('cart'))
 }
+
+
+//COMPRAR CARRITO
+
+document.addEventListener("click", (e) => {
+    if(e.target && e.target.matches("button.buyCart")){
+        buyTotal(e);
+        Swal.fire({
+            title: 'Ingrese sus datos',
+            confirmButtonColor: 'green',
+        })
+    }
+})
+
+
+function buyTotal() {
+    const total = `
+    <form id="form">
+        <div class="nombre">
+            <label for="nombre">Nombre y Apellido:</label>
+            <input type="text" id="nombre" placeholder="ingrese nombre y apellido">
+        </div>  
+        <div class="direccion">
+            <label for="direccion">Direccion:</label>
+            <input type="text" id="direccion" placeholder="ingrese direccion">
+        </div> 
+        <div class="numero">
+            <label for="numero">Numero de Celular:</label>
+            <input type="number" id="numero" placeholder="ingrese numero">
+        </div> 
+        <div class="email">
+            <label for="email">E-mail:</label>
+            <input type="mail" id="email" placeholder="ingrese e-mail">
+        </div>  
+        <button class="boton-enviar" type="submit">Enviar</button>
+    </form>
+    ` 
+    showCart.innerHTML += total;
+    
+}
+
+
+//CAPTURAR DATOS DEL USUARIO 
+
+document.addEventListener("click", (e) => {
+    if(e.target && e.target.matches("button.boton-enviar")){
+        e.preventDefault();
+        const nombre = document.getElementById('nombre')
+        const input1 = nombre.value;
+        const direccion = document.getElementById('direccion')
+        const input2 = direccion.value;
+        Swal.fire({
+            title: 'Muchas gracias por tu compra '  +  input1 + ' enviaremos tu pedido a ' + input2 + ' por correo.',
+            confirmButtonColor: 'green',
+            
+        })
+        form.reset()
+        tbody.innerHTML = ` `
+      
+    }
+        
+})
+
+
+
+
+
+
